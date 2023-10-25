eef343b54eb58eb767a1c14d6114d3c1dcb76767
seafood12 <abieledelso@gmail.com>
2023-10-26 00:25:06 +0100

    favicon

--- src/components/Plan.tsx
+++ src/components/Plan.tsx
@@ -9,7 +9,7 @@ function Plan ({ icon, name, price }: { icon: string, name: string, price: numbe
     <div onClick={() => { actions.setPlan({ name, price }) }}
       className={`plan cursor-pointer flex md:flex-col h-[80px] md:h-[160px] border  px-4 py-2 md:py-4 rounded-lg   gap-1 md:w-[130px] pr-12 
       ${snap.selectedPlan.name === name ? ' bg-[#e7eafd] border-[1px] border-[#483EFF] ' : ''} `} >
-      <img className='w-10 md:pb-6' src={icon} alt="" />
+      <img className='w-10 md:pb-6' src={icon} alt="plan logo" />
       <div className=' flex flex-col ml-6 md:ml-0'>
         <strong className='capitalize'>{name}</strong>
         {/* PRICE */}
