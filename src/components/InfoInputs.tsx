e93bb3636031876f250f216a69d1a4ebd722d733
seafood12 <abieledelso@gmail.com>
2023-10-26 01:17:11 +0100

    Update InfoInputs.tsx

--- src/components/InfoInputs.tsx
+++ src/components/InfoInputs.tsx
@@ -1,7 +1,7 @@
 function InfoInput ({ value, error, setValue, name }: InfoInputProps): JSX.Element {
   return (
     <div className='flex flex-col relative'>
-      <label className="font-medium capitalize" htmlFor="">{name}</label>
+      <label className="font-medium capitalize" >{name}</label>
       <input value={value} onChange={(e) => { setValue(e) }}
         placeholder='e.g. Stephen King'
         className="form-input  mb-8 mt-1 px-4 py-2 bg-transparent border-[#D6D9E6] border-2 rounded-lg" type="text" />
